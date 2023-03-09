b01e6f27079da53660e321c9ee1e4f2f4a5ed7d4
Jeet Vyas <118108655+JeetVyas25@users.noreply.github.com>
2023-03-09 22:35:26 +0530

    Add files via upload

---
+++ project1/src/components/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-100% h-fit   text-black px-20'>
+        <div className=' p-4 flex flex-col justify-left w-full h-full text-sm font-light mx-auto'>
+            <div className='pb-4'>
+                <h2 className='inline font-bold text-4xl border-b-4 border-yellow-600 text-yellow-500'>About Us</h2>
+            </div>
+            <div className='pb-2 text-base font-medium'>
+            Spear-headed by Adv. Jayant Bhatt, the Chamber provides litigation-related diligence, counseling, and solutions. His office caters to varied clients comprising of Governmental and Non-Governmental Bodies, Multinationals, Financials Institutions and individuals in an array of matters.
+            </div>
+            <p className='mt-3'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa vitae tortor condimentum lacinia quis. Ultricies mi quis hendrerit dolor magna eget est lorem ipsum. Lorem donec massa sapien faucibus et molestie ac feugiat sed. Commodo ullamcorper a lacus vestibulum sed arcu non odio euismod.<br></br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
